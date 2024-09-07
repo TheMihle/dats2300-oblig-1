2591092663cde0acb3acb4cc902775af475f5e2e
TheMihle <32242447+TheMihle@users.noreply.github.com>
2024-09-07 14:31:29 +0200

    Changed wrapping if to return if in task 5

--- src/main/java/no/oslomet/cs/algdat/Oblig1.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig1.java
@@ -50,17 +50,17 @@ public class Oblig1 {
     // Oppgave 5
     // Moves all characters of array to the right, and puts the rightmost element first. "Rotating" the array by one.
     public static void rotasjon(char[] a) {
-        if (a.length > 0) {
-            char last = a[a.length-1];
-            for (int i = a.length-1; i > 0; i--) {
-                a[i] = a[i-1];
-            }
-            a[0] = last;
+        if (a.length <= 0) return;
+
+        char last = a[a.length-1];
+        for (int i = a.length-1; i > 0; i--) {
+            a[i] = a[i-1];
         }
+        a[0] = last;
     }
 
      public static <T> void switchPlces(T[] array, int a, int b) {
-         T temp = array[a];
+        T temp = array[a];
         array[a] = array[b];
         array[b] = temp;
     }
